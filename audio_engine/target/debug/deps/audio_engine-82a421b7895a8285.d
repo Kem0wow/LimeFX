9c49820805753835b47c19eb27bddf0ad2b22a43
Kem0 <kdtastan@gmail.com>
2026-01-17 22:49:14 +0300

    dependencies

--- audio_engine/target/debug/deps/audio_engine-82a421b7895a8285.d
+++
@@ -1,6 +0,0 @@
-C:\Users\kdtas\Belgeler\GitHub\LimeFX\audio_engine\target\debug\deps\audio_engine-82a421b7895a8285.d: src\main.rs src\audio.rs
-
-C:\Users\kdtas\Belgeler\GitHub\LimeFX\audio_engine\target\debug\deps\libaudio_engine-82a421b7895a8285.rmeta: src\main.rs src\audio.rs
-
-src\main.rs:
-src\audio.rs:
--- audio_engine/target/debug/deps/audio_engine-c41624d8d36759f2.d
+++
@@ -1,6 +0,0 @@
-C:\Users\kdtas\Belgeler\GitHub\LimeFX\audio_engine\target\debug\deps\audio_engine-c41624d8d36759f2.d: src\main.rs src\audio.rs
-
-C:\Users\kdtas\Belgeler\GitHub\LimeFX\audio_engine\target\debug\deps\libaudio_engine-c41624d8d36759f2.rmeta: src\main.rs src\audio.rs
-
-src\main.rs:
-src\audio.rs:
